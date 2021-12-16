9422afbcf21785049d8528092950b29410e8c05a
mazeneleabd <mazenelabd98@gmail.com>
2021-12-16 13:02:11 +0200

    (create) Projects component

---
+++ components/projects/ProjectData.js
@@ -0,0 +1,54 @@
+export const PROJECTS = [
+  {
+    title: 'Real Estate Firm Website',
+    image: '/projects/1.png',
+    technologies: ['React', 'Next.js', 'Strapi', 'Cypress'],
+    website: 'https://real-estate-murex.vercel.app/',
+    github: 'https://github.com/mazenelabd/real-estate',
+  },
+  {
+    title: 'Productivity App',
+    image: '/projects/2.png',
+    technologies: [
+      'React',
+      'Node.js',
+      'Express.js',
+      'MongoDB',
+      'Redux',
+      'JWT',
+      'Cypress',
+    ],
+    website: 'https://productivity1.herokuapp.com/',
+    github: 'https://github.com/mazenelabd/Productivity',
+  },
+  {
+    title: 'Taylor Fans eCommerce Platform',
+    image: '/projects/3.png',
+    technologies: [
+      'React',
+      'Node.js',
+      'Express.js',
+      'MongoDB',
+      'Redux',
+      'JWT',
+      'Cypress',
+    ],
+    website: 'https://taylorfans.herokuapp.com',
+    github: 'https://github.com/mazenelabd/eCommerce-MERN',
+  },
+  {
+    title: 'Weather App + 5 days forecast',
+    image: '/projects/4.png',
+    technologies: ['React', 'React Testing Library', 'Jest'],
+    website: 'https://weather-5.netlify.app/',
+    github: 'https://github.com/mazenelabd/weather-app',
+  },
+  {
+    title: 'Photographer Frontend Template - 11 Pages',
+    image: '/projects/5.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap'],
+    website:
+      'https://mazenelabd.github.io/Photographer-Frontend-Template-/index.html',
+    github: 'https://github.com/mazenelabd/Photographer-Frontend-Template-',
+  },
+]
